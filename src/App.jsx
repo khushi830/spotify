5efbbcda29c93a234f68d1e286ebb31c7ecf8721
khushi <khushiiii4321@gmail.com>
2025-01-10 18:27:20 +0530

    starting the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+
+const App = () =>{
+  <main className="relative">
+    <Navbar/>
+    <section >
+      Hero
+    </section>
+
+  </main>
+}
+
+export default App;
