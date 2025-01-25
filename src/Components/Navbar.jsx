8af85e9eb020a868df82fbfce1f1f14556cf97f4
Khushi <khushiiii4321@gmail.com>
2025-01-26 01:46:45 +0530

    add changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,22 +2,13 @@ import { useState } from "react";
 import { Logo, Hamburger, Spotify } from "../assets";
 import { Home, Viewbox, Search, Install, Cross, Songsign } from "../assets";
 
-
 const Navbar = () => {
     const [Isopen, setIsopen] = useState(false);
     const togglemenu = () => {
         setIsopen(!Isopen);
     };
-    const navItems = [
-        { id: 1, text: 'Spotify' },
-        { id: 2, text: 'Home' },
-        { id: 3, text: 'Search' },
-
-
-    ];
-
     return (
-        <div className="flex cursor-auto justify-between items-center sticky top-0 z-[20] -m-4 bg-[#000000] w-full h-[64px]  p-[12px]">
+        <div className="flex justify-between items-center sticky top-0 z-[20] -m-4 bg-[#000000] w-full h-[64px]  p-[12px]">
 
             <div className="hidden md:block">
                 <div className="  flex items-center">
@@ -78,43 +69,54 @@ const Navbar = () => {
                     </button>
                 </div>
             </span>
-            <div className="  flex justify-center md:hidden items-center">
-                <a href="/" className="flex justify-center items-center gap-6">
+
+
+            <div className="flex md:hidden items-center">
+                <button onClick={togglemenu} className="flex items-center gap-6">
                     <button className="bg-[#FFFFFF] text-[#000000] px-[20px] py-[8px]  font-bold rounded-full">
                         Open App
                     </button>
-                    <button onClick={togglemenu} style={{ zIndex: 1000 }}>
-                        <img src={Isopen ? Cross : Hamburger} alt="hamburger" className="w-[2rem]" />
-                    </button>
-
-                </a>
+                    <img
+                        src={Isopen ? Cross : Hamburger}
+                        alt="hamburger"
+                        className="w-[2rem]"
+                    />
+                </button>
             </div>
-            {Isopen ? (
-                <div
-                    className={`fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 ${Isopen ? "translate-x-0" : "-translate-x-full"
-                        }`}
+
+
+            <div
+                className={`fixed top-0 left-0 w-full h-full bg-[#000000] text-white z-200 transform ${Isopen ? "translate-x-0" : "translate-x-full"
+                    } transition-transform duration-500 ease-in-out md:hidden`}
+            >
+                <button
+                    onClick={togglemenu}
+                    className="absolute top-4 right-4 text-white"
                 >
-                    <a href="/" className="mb-2 text-white font-bold hover:text-gray-400 flex items-center">
-                        <img src={Logo} alt="Home" className="w-5 h-5 mr-2" />
-                        Spotify
+                    <img src={Cross} alt="close" className="w-[2rem]" />
+                </button>
+                <nav className="flex flex-col mt-32 p-6">
+                    <a href="/" className="flex justify-center items-center  hover:text-gray-400 text-[#fff]  font-bold p-[4px]  mx-2">
+                        <img src={Logo} alt="logo" className="w-[2rem]" />
+                        <span>Spotify</span>
                     </a>
-                    <a href="/" className="mb-2 text-white hover:text-gray-400 flex items-center">
+                    <a
+                        href="/"
+                        className="flex justify-center items-center  hover:text-gray-400 text-[#fff]   p-[4px]  mx-2"
+                    >
                         <img src={Home} alt="Home" className="w-5 h-5 mr-2" />
                         Home
                     </a>
-                    <a href="/search" className="mb-2 text-white hover:text-gray-400 flex items-center">
-                        <img src={Search} alt="Search" className="w-5 h-5 mr-2" />
-                        Search
+                   
+                    <a
+                        href="/"
+                        className="flex  justify-center items-center  hover:text-gray-400 text-[#fff]   p-[4px] mt-16 mx-2"
+                    >
+                        <img src={Songsign} alt="Playing Song" className="w-5 h-5 mr-2" />
+                        Playing Song
                     </a>
-                    <a href="/play" className="text-white hover:text-gray-400 flex items-center">
-                        <img src={Songsign} alt="Play Song" className="w-5 h-5 mr-2" />
-                        Play Song
-                    </a>
-                </div>
-            ) : null}
-
-
-
+                </nav>
+            </div>
         </div>
     );
 };
